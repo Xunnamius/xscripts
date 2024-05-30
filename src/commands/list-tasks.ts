ed94c5f1c88f9c0129728197cbb7cb327ba7adb4
Xunnamius (Romulus) <Xunnamius@users.noreply.github.com>
2024-05-30 15:49:22 -0700

    refactor(src): stub out interface shape

---
+++ src/commands/list-tasks.ts
@@ -0,0 +1,42 @@
+import { ChildConfiguration } from '@black-flag/core';
+
+import { CustomExecutionContext } from 'universe/configure';
+import { LogTag, standardSuccessMessage } from 'universe/constant';
+
+import {
+  GlobalCliArguments,
+  logStartTime,
+  makeUsageString,
+  withGlobalOptions,
+  withGlobalOptionsHandling
+} from 'universe/util';
+
+export type CustomCliArguments = GlobalCliArguments;
+
+export { command };
+export default async function command({
+  log: genericLogger,
+  debug_,
+  state
+}: CustomExecutionContext) {
+  const [builder, builderData] = await withGlobalOptions<CustomCliArguments>({});
+
+  return {
+    builder,
+    description: 'List all tasks (typically NPM scripts) supported by this project',
+    usage: makeUsageString(),
+    handler: await withGlobalOptionsHandling<CustomCliArguments>(
+      builderData,
+      async function () {
+        const debug = debug_.extend('handler');
+        debug('entered handler');
+
+        const { startTime } = state;
+
+        logStartTime({ log: genericLogger, startTime });
+
+        genericLogger([LogTag.IF_NOT_QUIETED], standardSuccessMessage);
+      }
+    )
+  } satisfies ChildConfiguration<CustomCliArguments, CustomExecutionContext>;
+}
