dc835542adf8f69e75340c553ea07e9eaedb0a44
Xunnamius (Romulus) <Xunnamius@users.noreply.github.com>
2024-06-12 19:22:39 -0700

    refactor(src): use @-xun/cli-utils and @black-flag/extensions

--- src/configure.ts
+++ src/configure.ts
@@ -1,137 +1,25 @@
-import { isNativeError } from 'node:util/types';
-
-import { type ExecutionContext } from '@black-flag/core/util';
+import { globalDebuggerNamespace, globalLoggerNamespace } from 'universe/constant';
 
 import {
-  createDebugLogger,
-  createGenericLogger,
-  ExtendedDebugger,
-  TAB,
-  type ExtendedLogger
-} from 'multiverse/rejoinder';
+  makeStandardConfigureErrorHandlingEpilogue,
+  makeStandardConfigureExecutionContext
+} from 'multiverse/@-xun/cli-utils/configure';
 
-import {
-  globalDebuggerNamespace,
-  globalLoggerNamespace,
-  LogTag,
-  MAX_LOG_ERROR_ENTRIES
-} from 'universe/constant';
+import { type StandardExecutionContext } from 'multiverse/@-xun/cli-utils/index';
 
-import { TaskError } from 'universe/error';
-import { toFirstLowerCase } from 'universe/util';
+import { createDebugLogger, createGenericLogger } from 'multiverse/rejoinder';
 
-import type {
-  ConfigureErrorHandlingEpilogue,
-  ConfigureExecutionContext
-} from '@black-flag/core';
-
-const { IF_NOT_SILENCED, IF_NOT_QUIETED } = LogTag;
 const rootGenericLogger = createGenericLogger({ namespace: globalLoggerNamespace });
 const rootDebugLogger = createDebugLogger({ namespace: globalDebuggerNamespace });
 
 export { $executionContext } from '@black-flag/core';
 
-export type CustomExecutionContext = ExecutionContext & {
-  /**
-   * The {@link ExtendedLogger} for the CLI.
-   */
-  log: ExtendedLogger;
-  /**
-   * The {@link ExtendedDebugger} for the CLI.
-   */
-  debug_: ExtendedDebugger;
-  state: {
-    /**
-     * If `true`, the program should not output anything at all. It also implies
-     * `isQuieted` and `isHushed` are both `true`.
-     */
-    isSilenced: boolean;
-    /**
-     * If `true`, the program should be dramatically less verbose. It also
-     * implies `isHushed` is `true`.
-     */
-    isQuieted: boolean;
-    /**
-     * If `true`, the program should output only the most pertinent information.
-     */
-    isHushed: boolean;
-    /**
-     * A `Date` object representing the start time of execution.
-     */
-    startTime: Date;
-  };
-};
+export type CustomExecutionContext = StandardExecutionContext;
 
-export const configureExecutionContext: ConfigureExecutionContext = (context) => {
-  return {
-    ...context,
-    log: rootGenericLogger,
-    debug_: rootDebugLogger,
-    state: {
-      ...context.state,
-      isSilenced: false,
-      isQuieted: false,
-      isHushed: false,
-      startTime: new Date()
-    }
-  };
-};
+export const configureExecutionContext = makeStandardConfigureExecutionContext({
+  rootGenericLogger,
+  rootDebugLogger
+});
 
-export const configureErrorHandlingEpilogue: ConfigureErrorHandlingEpilogue<
-  CustomExecutionContext
-> = async (
-  ...[{ message, error }, _argv, context]: Parameters<
-    ConfigureErrorHandlingEpilogue<CustomExecutionContext>
-  >
-) => {
-  // ? Pretty print error output depending on how silent we're supposed to be
-  if (!context.state.isSilenced) {
-    if (context.state.didOutputHelpOrVersionText) {
-      context.log.newline([IF_NOT_SILENCED], 'alternate');
-    }
-
-    if (message) {
-      context.log.error(
-        [IF_NOT_SILENCED],
-        `❌ Execution failed: ${toFirstLowerCase(message)}`
-      );
-    }
-
-    if (
-      !context.state.isQuieted &&
-      isNativeError(error) &&
-      error.cause &&
-      // ? Don't repeat what has already been output
-      error.cause !== message
-    ) {
-      for (
-        let count = 0, subError: Error | undefined = error;
-        subError?.cause && count < MAX_LOG_ERROR_ENTRIES;
-        count++
-      ) {
-        if (isNativeError(subError.cause)) {
-          if (count === 0) {
-            if (!subError.cause.cause) {
-              break;
-            }
-
-            context.log.error([IF_NOT_QUIETED], '❌ Causal stack:');
-          }
-
-          context.log.error(
-            [IF_NOT_QUIETED],
-            `${TAB}⮕  ${subError.cause instanceof TaskError ? toFirstLowerCase(subError.cause.message) : subError.cause.message}`
-          );
-          subError = subError.cause;
-        } else {
-          context.log.error([IF_NOT_QUIETED], `${TAB}⮕  ${String(subError.cause)}`);
-          subError = undefined;
-        }
-
-        if (count + 1 >= MAX_LOG_ERROR_ENTRIES) {
-          context.log.error([IF_NOT_QUIETED], `(remaining entries have been hidden)`);
-        }
-      }
-    }
-  }
-};
+export const configureErrorHandlingEpilogue =
+  makeStandardConfigureErrorHandlingEpilogue();
--- src/error.ts
+++ src/error.ts
@@ -1,13 +1,7 @@
-import { ErrorMessage as UpstreamErrorMessage } from '@black-flag/core/util';
-import { toSentenceCase, type ProjectMetaAttribute } from 'universe/util';
-/**
- * An `Error` class where the first letter of the message is capitalized.
- */
-export class TaskError extends Error {
-  constructor(...args: Required<ConstructorParameters<typeof Error>>) {
-    super(toSentenceCase(args[0]), args[1]);
-  }
-}
+import { ErrorMessage as UpstreamErrorMessage } from 'multiverse/@-xun/cli-utils/error';
+import { type ProjectMetaAttribute } from 'universe/util';
+
+export { TaskError } from 'multiverse/@-xun/cli-utils/error';
 
 /**
  * A collection of possible error and warning messages.
@@ -21,39 +15,16 @@ export const ErrorMessage = {
   AssertionFailureBadPackageJson() {
     return 'assertion failed: cannot load given package.json file';
   },
-  AssertionFailureCannotUseDoubleFeature() {
-    return 'assertion failed: cannot use both special options features at once';
-  },
-  AssertionFailureUnequalDemandOptions() {
-    return 'assertion failed: special demandOptions array feature requires matching arrays';
-  },
   AssertionFailureCannotBeCliAndNextJs() {
     return 'assertion failed: project must either provide a CLI or be a Next.js project';
   },
-  AssertionFailureBadArgumentCombination(firstArgument: string, secondArgument: string) {
-    return `cannot provide both "${firstArgument}" and "${secondArgument}" arguments`;
-  },
   CleanCalledWithoutForce() {
     return 'no deletions were performed (try again with --force)';
-  },
-  UnsupportedCommand() {
-    return 'this project does not support this command';
-  },
-  IgnoredArguments(args: string[]) {
-    return `the following command arguments were ignored: ${args.join(', ')}`;
   },
   WrongProjectAttributes(
     expected: ProjectMetaAttribute[],
     actual: ProjectMetaAttribute[]
   ) {
     return `expected a project with the following attributes: ${expected.join(', ')}; saw ${actual.join(', ')} instead`;
-  },
-  DidNotProvideAtLeastOneOfSeveralOptions(givenOptions: Record<string, unknown>) {
-    const possibleOptions = Object.keys(givenOptions);
-    return `at least one of the following options must be provided: ${possibleOptions.join(', ')}`;
-  },
-  DidNotProvideExactlyOneOfSeveralOptions(givenOptions: Record<string, unknown>) {
-    const possibleOptions = Object.keys(givenOptions);
-    return `exactly one of the following options must be provided: ${possibleOptions.join(', ')}`;
   }
 };
