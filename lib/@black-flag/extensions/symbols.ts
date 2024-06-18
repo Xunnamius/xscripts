17d53c3b83fc6ed799b5b2ab1da5feefe4e37018
Xunnamius (Romulus) <Xunnamius@users.noreply.github.com>
2024-06-18 16:04:03 -0700

    fix(@black-flag/extensions): add missing symbols

---
+++ lib/@black-flag/extensions/symbols.ts
@@ -0,0 +1,9 @@
+/**
+ * Represents any value for the given argument.
+ */
+export const $exists = Symbol('exists');
+
+/**
+ * Represents an argument that is the genesis of some logical implication.
+ */
+export const $genesis = Symbol('genesis');
