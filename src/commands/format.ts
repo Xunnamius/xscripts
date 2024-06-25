1dac13e6546f53f3d4344ea065352e352fac5d90
Xunnamius (Romulus) <Xunnamius@users.noreply.github.com>
2024-06-25 00:26:00 -0700

    refactor(src): add struts for "format" command [WIP]

--- src/commands/format.ts
+++ src/commands/format.ts
@@ -14,23 +14,46 @@ import {
 } from 'multiverse/@-xun/cli-utils/extensions';
 
 import { scriptBasename } from 'multiverse/@-xun/cli-utils/util';
+import { findMarkdownFiles } from 'universe/util';
 
-export type CustomCliArguments = GlobalCliArguments;
+export type CustomCliArguments = GlobalCliArguments & {
+  'sort-package-json': boolean;
+  'skip-docs': boolean;
+  'renumber-references': boolean;
+};
 
 export default function command({ log, debug_, state }: GlobalExecutionContext) {
   const [builder, withStandardHandler] = withStandardBuilder<
     CustomCliArguments,
     GlobalExecutionContext
   >({
-    // TODO (skip docs)
-    // TODO (do not run renumbering unless given flag for it; add flag to deploy)
+    'sort-package-json': {
+      boolean: true,
+      description: 'Sort any package.json files',
+      default: true
+    },
+    'skip-docs': {
+      boolean: true,
+      description: 'Do not format any files with /doc/ in their path',
+      default: true
+    },
+    'renumber-references': {
+      boolean: true,
+      description: 'Run the renumber-references plugin when formatting Markdown files',
+      default: false
+    }
   });
 
   return {
     builder,
     description: 'Run formatters (e.g. prettier, remark) across all relevant files',
     usage: withStandardUsage(),
-    handler: withStandardHandler(async function ({ $0: scriptFullName }) {
+    handler: withStandardHandler(async function ({
+      $0: scriptFullName,
+      sortPackageJson,
+      skipDocs,
+      renumberReferences
+    }) {
       const genericLogger = log.extend(scriptBasename(scriptFullName));
       const debug = debug_.extend('handler');
       debug('entered handler');
@@ -38,6 +61,11 @@ export default function command({ log, debug_, state }: GlobalExecutionContext) 
       const { startTime } = state;
 
       logStartTime({ log, startTime });
+
+      const mdFiles = await findMarkdownFiles();
+      genericLogger([LogTag.IF_NOT_QUIETED], 'mdFiles: %O', mdFiles);
+
+      void sortPackageJson, skipDocs, renumberReferences;
 
       genericLogger([LogTag.IF_NOT_QUIETED], standardSuccessMessage);
     })
