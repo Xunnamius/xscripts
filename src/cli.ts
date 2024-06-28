ec7a6a2ccbbd4282532b2b31d0b3314824f5d205
Xunnamius (Romulus) <Xunnamius@users.noreply.github.com>
2024-06-28 07:17:29 -0700

    refactor(src): remove suppress-warnings usage

--- src/cli.ts
+++ src/cli.ts
@@ -3,11 +3,11 @@
 import { join } from 'node:path';
 
 import { runProgram } from '@black-flag/core';
-import { suppressNodeWarnings } from 'multiverse/suppress-warnings';
+//import { suppressNodeWarnings } from 'multiverse/suppress-warnings';
 
 import type { GlobalExecutionContext } from 'universe/configure';
 
-suppressNodeWarnings('ExperimentalWarning');
+//suppressNodeWarnings('ExperimentalWarning');
 
 /**
  * This is the simple CLI entry point executed directly by node.
