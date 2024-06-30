2bd7202c4bdd8a5f5af004faf67dbfcbf93651df
Xunnamius (Romulus) <Xunnamius@users.noreply.github.com>
2024-06-29 17:41:05 -0700

    refactor(src): add --silently-ignore to remark just in case

--- src/commands/format.ts
+++ src/commands/format.ts
@@ -269,6 +269,7 @@ export default function command({
               ...(isHushed ? ['--quiet'] : []),
               '--output',
               '--frail',
+              '--silently-ignore',
               ...mdFiles
             ],
             {
