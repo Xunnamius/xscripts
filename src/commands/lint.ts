346b4ac5d27ea045cd037c4987401786f7fa572b
Xunnamius (Romulus) <Xunnamius@users.noreply.github.com>
2024-07-08 12:07:52 -0700

    feat(src): implement "lint" command

--- src/commands/lint.ts
+++ src/commands/lint.ts
@@ -1,7 +1,12 @@
 import { type ChildConfiguration } from '@black-flag/core';
 
 import { type GlobalCliArguments, type GlobalExecutionContext } from 'universe/configure';
-import { globalPreChecks } from 'universe/util';
+import { ErrorMessage } from 'universe/error';
+import {
+  checkAllChoiceIfGivenIsByItself,
+  findProjectFiles,
+  globalPreChecks
+} from 'universe/util';
 
 import {
   LogTag,
@@ -16,9 +21,23 @@ import {
 
 import { scriptBasename } from 'multiverse/@-xun/cli-utils/util';
 import { type AsStrictExecutionContext } from 'multiverse/@black-flag/extensions';
+import { run } from 'multiverse/run';
+
+export enum Linter {
+  Tsc = 'tsc',
+  Eslint = 'eslint',
+  Remark = 'remark',
+  All = 'all'
+}
+
+export const linters = Object.values(Linter);
+
+export const limitedScopeDirectories = ['types', 'lib', 'src', 'external-scripts'];
 
 export type CustomCliArguments = GlobalCliArguments & {
-  // TODO
+  linter: Linter[];
+  scope: 'limited' | 'all';
+  remarkSkipIgnored: boolean;
 };
 
 export default function command({
@@ -31,26 +50,40 @@ export default function command({
     CustomCliArguments,
     GlobalExecutionContext
   >({
-    // TODO: split these TODOs between this command and the "project lint" command
-    //
-    // TODO (run all linting simultaneously)
-    // TODO (no dot imports outside lib directory)
-    // TODO (bring in lint code from projector)
-    // TODO (add depcheck package (maybe), browserslist (update caniuse first), attw, and others into the mix)
-    // TODO (test dir cannot appear under src, directly under lib, or under external-scripts)
-    // TODO (latest versions of all files being used, including lint-staged et al)
-    // TODO (ensure all imported packages in dist are)
-    // TODO (lint project structure)
-    // TODO (lint engines.node with browserslist 'maintained node versions, current node' with flag for allowing lower bound drop off (i.e. allow lower versions than given to be missing))
-    // TODO (two linting stages: "early lint" (the usual) and "late lint" (that checks all prod deps resolvable, no extraneous deps or dev deps, no missing deps))
-    // TODO (npx update-browserslist-db@latest (non-match doesn't cause error only warning))
+    linter: {
+      alias: 'linters',
+      array: true,
+      choices: linters,
+      description: 'Which linters to run',
+      default: [Linter.All],
+      check: checkAllChoiceIfGivenIsByItself(Linter.All, 'linter value')
+    },
+    scope: {
+      choices: ['limited', 'all'],
+      description: 'Limit linting to transpilation targets or include all source files',
+      default: 'limited'
+    },
+    'remark-skip-ignored': {
+      boolean: true,
+      description: 'Ignore files listed in .prettierignore when running remark linter',
+      default: true
+    }
   });
 
   return {
     builder,
     description: 'Run linters (e.g. eslint, remark) across all relevant files',
-    usage: withStandardUsage(),
-    handler: withStandardHandler(async function ({ $0: scriptFullName }) {
+    usage: withStandardUsage(
+      `$1.\n\nPassing --scope=limited will exclude from linting (by eslint) all files that are not under the following directories: ${limitedScopeDirectories.join(', ')}. For tsc, the scope of linted files is always determined by the "includes" and "excludes" directives in the relevant tsconfig file.`
+    ),
+    handler: withStandardHandler(async function ({
+      $0: scriptFullName,
+      linter: linters,
+      scope,
+      remarkSkipIgnored: skipIgnored,
+      hush: isHushed,
+      quiet: isQuieted
+    }) {
       const genericLogger = log.extend(scriptBasename(scriptFullName));
       const debug = debug_.extend('handler');
 
@@ -62,6 +95,87 @@ export default function command({
 
       logStartTime({ log, startTime });
       genericLogger([LogTag.IF_NOT_QUIETED], 'Linting project...');
+
+      debug('linters: %O', linters);
+      debug('scope: %O', scope);
+      debug('skipIgnored: %O', skipIgnored);
+
+      const {
+        project: {
+          // ? This does NOT end in a slash and this must be taken into account!
+          root: rootDir
+        }
+      } = runtimeContext;
+
+      debug('rootDir: %O', rootDir);
+
+      const allLinters = linters.includes(Linter.All);
+      debug('allLinters: %O', allLinters);
+
+      // ? Widen the aperture to all lintable files if it is demanded
+      const tsconfigFilePath =
+        scope === 'all' ? `tsconfig.eslint.json` : `tsconfig.lint.json`;
+
+      debug('tsconfigFilePath: %O', tsconfigFilePath);
+
+      const promisedLinters: Promise<unknown>[] = [];
+
+      if (allLinters || linters.includes(Linter.Tsc)) {
+        debug('running tsc');
+        promisedLinters.push(
+          run('npx', ['tsc', '--project', tsconfigFilePath], {
+            stdout: isHushed ? 'ignore' : 'inherit',
+            stderr: isQuieted ? 'ignore' : 'inherit'
+          })
+        );
+      }
+
+      if (allLinters || linters.includes(Linter.Eslint)) {
+        debug('running eslint');
+        promisedLinters.push(
+          run(
+            'npx',
+            [
+              'eslint',
+              `--parser-options=project:${tsconfigFilePath}`,
+              '--no-error-on-unmatched-pattern',
+              '--max-warnings=0',
+              ...(scope === 'all' ? ['.'] : limitedScopeDirectories)
+            ],
+            {
+              stdout: isHushed ? 'ignore' : 'inherit',
+              stderr: isQuieted ? 'ignore' : 'inherit'
+            }
+          )
+        );
+      }
+
+      if (allLinters || linters.includes(Linter.Remark)) {
+        debug('running remark');
+        const { mdFiles } = await findProjectFiles(runtimeContext, { skipIgnored });
+
+        promisedLinters.push(
+          run(
+            'npx',
+            [
+              'remark',
+              ...(isHushed ? ['--quiet'] : []),
+              '--frail',
+              '--no-stdout',
+              '--silently-ignore',
+              ...mdFiles
+            ],
+            {
+              env: { NODE_ENV: 'lint' },
+              stdout: isHushed ? 'ignore' : 'inherit',
+              stderr: isQuieted ? 'ignore' : 'inherit'
+            }
+          )
+        );
+      }
+
+      debug('waiting for linters to finish running...');
+      await Promise.all(promisedLinters);
 
       genericLogger([LogTag.IF_NOT_QUIETED], standardSuccessMessage);
     })
