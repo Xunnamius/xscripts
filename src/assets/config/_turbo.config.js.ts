2ec3395ff836ae90a6b27d54aff0ac802b5d6d02
Xunnamius (Romulus) <Xunnamius@users.noreply.github.com>
2024-07-27 15:23:39 -0700

    refactor(src): add turbo configuration asset

---
+++ src/assets/config/_turbo.config.js.ts
@@ -0,0 +1,28 @@
+import { assertIsExpectedTransformerContext, makeTransformer } from 'universe/assets';
+import { globalDebuggerNamespace } from 'universe/constant';
+
+import type { EmptyObject } from 'type-fest';
+
+export type Context = EmptyObject;
+
+export const { transformer } = makeTransformer<Context>({
+  transform(context) {
+    const { name } = assertIsExpectedTransformerContext(context);
+
+    return {
+      [name]: /*js*/ `
+'use strict';
+
+// TODO: publish latest rejoinder package first, then update configs to use it
+/*const { createDebugLogger } = require('debug-extended');
+const debug = createDebugLogger({
+  namespace: '${globalDebuggerNamespace}:config:conventional'
+});*/
+
+// TODO
+
+/*debug('exported config: %O', module.exports);*/
+`.trimStart()
+    };
+  }
+});
