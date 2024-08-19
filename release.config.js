6ef0123a0d9d1668ce567cf526e04951a3d25dd1
Xunnamius (Romulus) <Xunnamius@users.noreply.github.com>
2024-08-19 07:58:00 -0700

    feat(release): support modern changelog generation flow

--- release.config.js
+++ release.config.js
@@ -1,5 +1,10 @@
 'use strict';
 
+const assert = require('node:assert');
+const os = require('node:os');
+const path = require('node:path');
+const crypto = require('node:crypto');
+
 const debug = require('debug')(
   `${require('./package.json').name}:semantic-release-config`
 );
@@ -11,6 +16,44 @@ const updateChangelog = process.env.UPDATE_CHANGELOG !== 'false';
 debug(`will update changelog: ${updateChangelog ? 'yes' : 'no'}`);
 
 const { parserOpts, writerOpts } = require('./conventional.config');
+
+const tmpChangelogReleaseSectionPath = path.join(
+  os.tmpdir(),
+  'xscripts-release-changelog-' + crypto.randomBytes(4).readUInt32LE(0).toString(16)
+);
+
+debug(`tmpChangelogReleaseSectionPath: ${tmpChangelogReleaseSectionPath}`);
+
+const releaseBodyTemplate = /* js */ `
+try {
+  print(
+    require('node:fs')
+      .readFileSync('${tmpChangelogReleaseSectionPath}', 'utf8')
+      .trim() ||
+      'Failed to generate changelog: unexpectedly empty file: ${tmpChangelogReleaseSectionPath}'
+  );
+} catch (error) {
+  print('Failed to generate changelog: ' + String(error));
+}
+`.trim();
+
+debug(`releaseBodyTemplate: ${releaseBodyTemplate}`);
+
+// ! Cannot contain the single-quote character (')
+const cleanupTmpFilesTemplate = /* js */ `
+try {
+  require("node:fs").rmSync("${tmpChangelogReleaseSectionPath}", {
+    force: true
+  });
+} catch {}
+`.trim();
+
+debug(`cleanupTmpFilesTemplate: ${cleanupTmpFilesTemplate}`);
+
+assert(
+  !cleanupTmpFilesTemplate.includes("'"),
+  'release.config.js assertion failed: invalid cleanupTmpFilesTemplate value (hard-coded)'
+);
 
 module.exports = {
   branches: [
@@ -47,33 +90,50 @@ module.exports = {
         writerOpts
       }
     ],
-    // TODO: augment this so that the latest section gets put in here properly
-    // TODO: (write new section out, then read it in via scripts build changelog)
+    ['@semantic-release/changelog', { changelogFile: tmpChangelogReleaseSectionPath }],
     ...(updateChangelog
       ? [
           [
             '@semantic-release/exec',
             {
-              prepareCmd: 'NODE_NO_WARNINGS=1 npx xscripts build changelog'
+              prepareCmd: `NODE_NO_WARNINGS=1 npx xscripts build changelog --import-section-file ${tmpChangelogReleaseSectionPath}`
             }
           ]
         ]
       : []),
+    // ? We run this before @semantic-release/github so the GitHub release is
+    // ? patched. We run this after the updateChangelog section so that we
+    // ? don't run the patcher over the same file twice, which would be bad.
+    // * Note how we patch tmpChangelogReleaseSectionPath and not CHANGELOG.md.
+    [
+      '@semantic-release/exec',
+      {
+        prepareCmd: `NODE_NO_WARNINGS=1 npx xscripts build changelog --only-patch-changelog --no-format-changelog --output-file ${tmpChangelogReleaseSectionPath}`
+      }
+    ],
     [
       '@semantic-release/git',
       {
-        assets: [
-          'package.json',
-          'package-lock.json',
-          'npm-shrinkwrap.json',
-          'CHANGELOG.md',
-          'docs'
-        ],
+        assets: ['package.json', 'package-lock.json', 'CHANGELOG.md', 'docs'],
         message: 'release: ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
       }
     ],
+    // ! NPM and GitHub steps must be last just in case any other steps fail !
+    // TODO: add support for GitHub Actions build provenance attestations here
     ['@semantic-release/npm'],
-    ['@semantic-release/github']
+    [
+      '@semantic-release/github',
+      {
+        // ? Make sure semantic-release uses a patched release (changelog) body.
+        releaseBodyTemplate: `<% ${releaseBodyTemplate} %>`
+      }
+    ],
+    [
+      '@semantic-release/exec',
+      {
+        prepareCmd: `node --input-type commonjs --eval '${cleanupTmpFilesTemplate}'`
+      }
+    ]
   ]
 };
 
