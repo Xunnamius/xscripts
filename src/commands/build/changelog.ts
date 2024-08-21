8735f612072b02c3af08054d8f858b5764aab92d
Xunnamius (Romulus) <Xunnamius@users.noreply.github.com>
2024-08-21 01:08:38 -0700

    fix(src): make "build changelog" `CustomCliArguments` type more accurate

--- src/commands/build/changelog.ts
+++ src/commands/build/changelog.ts
@@ -73,7 +73,7 @@ export type CustomCliArguments = GlobalCliArguments & {
   onlyPatchChangelog: boolean;
   outputUnreleased: boolean;
   outputOrder: OutputOrder;
-  importSectionFile: string;
+  importSectionFile?: string;
   changelogFile: string;
 };
 
