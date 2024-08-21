d01832e0e20f168cfed2fe221fb756e6f502fab6
Xunnamius (Romulus) <Xunnamius@users.noreply.github.com>
2024-08-21 02:19:31 -0700

    chore: remove @semantic-release/release-notes-generator from dependencies (for now)

--- types/release-notes-generator.d.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-declare module '@semantic-release/release-notes-generator' {
-  import type { GenerateNotesContext } from 'semantic-release';
-
-  export async function generateNotes(
-    pluginConfig: { parserOpts: any; writerOpts: any },
-    context: GenerateNotesContext
-  ): Promise<string>;
-}
