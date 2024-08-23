967379af6dc0a4dfea358ed742cb3a7556fee21b
Xunnamius (Romulus) <Xunnamius@users.noreply.github.com>
2024-08-22 21:53:05 -0700

    refactor(@-xun/project-utils): restructure

--- lib/@-xun/project-utils/src/index.ts
+++ lib/@-xun/project-utils/src/index.ts
@@ -20,6 +20,12 @@ import {
 import type { GlobOptions } from 'glob';
 import type { PackageJsonWithConfig } from 'types/global';
 
+import { isAbsolute } from 'node:path';
+import semver from 'semver';
+import browserslist from 'browserslist';
+
+import { PathIsNotAbsoluteError } from 'pkgverse/core/src/errors';
+
 const _packageJsonReadCache = new Map<string, PackageJsonWithConfig>();
 
 export type WorkspacePackageName = string;
@@ -645,4 +651,63 @@ function _flattenPackageJsonSubpathMap(
           );
     });
   }
+}
+
+/**
+ * Throws if the provided path is not absolute.
+ */
+export function ensurePathIsAbsolute({ path }: { path: string }) {
+  if (!isAbsolute(path)) {
+    throw new PathIsNotAbsoluteError(path);
+  }
+}
+
+/**
+ * Returns the expected value for `package.json` `node.engines` field
+ */
+export function getMaintainedNodeVersions(options?: {
+  /**
+   * This determines in what format the results are returned. `"engines"`
+   * returns the currently maintained node versions as a string suitable for the
+   * `engines` key in a `package.json` file. `array` returns an array of the
+   * currently maintained node versions.
+   *
+   * @default engines
+   */
+  format?: 'engines';
+}): string;
+/**
+ * Returns the expected value for `package.json` `node.engines` field
+ */
+export function getMaintainedNodeVersions(options?: {
+  /**
+   * This determines in what format the results are returned. `"engines"`
+   * returns the currently maintained node versions as a string suitable for the
+   * `engines` key in a `package.json` file. `array` returns an array of the
+   * currently maintained node versions.
+   *
+   * @default engines
+   */
+  format?: 'array';
+}): string[];
+export function getMaintainedNodeVersions(options?: {
+  /**
+   * This determines in what format the results are returned. `"engines"`
+   * returns the currently maintained node versions as a string suitable for the
+   * `engines` key in a `package.json` file. `array` returns an array of the
+   * currently maintained node versions.
+   *
+   * @default engines
+   */
+  format?: 'engines' | 'array';
+}): string | string[] {
+  const versions = browserslist('maintained node versions')
+    .map((v) => v.split(' ').at(-1) as string)
+    .sort(semver.compareBuild);
+
+  return options?.format == 'array'
+    ? versions
+    : versions
+        .map((value, index, array) => `${index == array.length - 1 ? '>=' : '^'}${value}`)
+        .join(' || ');
 }
