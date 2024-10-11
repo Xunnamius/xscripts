8d69310b68b2362d815e1e1e1d76d5688d6b46ff
Xunnamius (Romulus) <Xunnamius@users.noreply.github.com>
2024-10-11 07:38:43 -0700

    build: prevent automatic updates of super-pinned packages

--- .ncurc.js
+++ .ncurc.js
@@ -1,8 +1,12 @@
+'use strict';
+
 // * https://www.npmjs.com/package/npm-check-updates#configuration-files
 
 module.exports = {
   reject: [
-    // ? Pin the CJS version
+    // ? Reject any super-pinned dependencies (e.g. find-up~5 and execa~7)
+    '*~*',
+    // ? Pin the CJS version (for the debug package)
     'supports-color',
     // ? Until upstream semantic-release-atam updates, this needs to be frozen
     '@semantic-release/release-notes-generator'
