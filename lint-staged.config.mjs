8833e0a06f0733e89b4496719aa8b71050783339
Xunnamius (Romulus) <Xunnamius@users.noreply.github.com>
2024-10-11 07:42:23 -0700

    build(lint-staged): update lint-staged configuration from cjs (js) to esm (mjs)

--- lint-staged.config.mjs
+++ lint-staged.config.mjs
@@ -1,12 +1,10 @@
-'use strict';
-
 /**
  * These scripts are the constituent parts of the `npm run format` command.
  *
  * ! Unlike other lint-staged scripts, since this project is itself xscripts,
  * ! we avoid using npx here. For any other project, npx should be used.
  */
-module.exports = {
+export default {
   '*.md': './node_modules/@-xun/scripts/dist/src/cli.js format --only-markdown --files',
   'package.json':
     './node_modules/@-xun/scripts/dist/src/cli.js format --only-package-json --files',
