a08f6855aa53f6e48b4a64ac177519ddd07904ed
Xunnamius (Romulus) <Xunnamius@users.noreply.github.com>
2024-10-18 10:49:19 -0700

    refactor(src): stub out "release"

--- src/commands/release.ts
+++ src/commands/release.ts
@@ -23,7 +23,12 @@ import { runGlobalPreChecks, withGlobalBuilder, withGlobalUsage } from 'universe
  */
 export const releaseScopes = Object.values(ReleaseScope);
 
-export type CustomCliArguments = GlobalCliArguments<ReleaseScope>;
+export type CustomCliArguments = GlobalCliArguments<ReleaseScope> & {
+  ci: boolean;
+  rebuildChangelog: boolean;
+  skipPrereleaseTasks: boolean;
+  synchronizeInterdependencies: boolean;
+};
 
 export default function command({
   log,
@@ -32,7 +37,31 @@ export default function command({
   projectMetadata: projectMetadata_
 }: AsStrictExecutionContext<GlobalExecutionContext>) {
   const [builder, withGlobalHandler] = withGlobalBuilder<CustomCliArguments>({
-    scope: { choices: releaseScopes }
+    scope: { choices: releaseScopes },
+    ci: {
+      alias: 'continuous-integration',
+      boolean: true,
+      description: 'Enable functionality for CI execution environments',
+      default:
+        process.env.NODE_ENV !== undefined && process.env.NODE_ENV !== 'development'
+    },
+    'rebuild-changelog': {
+      boolean: true,
+      description: 'Completely rebuild the changelog before release',
+      default: true
+    },
+    'skip-prerelease-tasks': {
+      boolean: true,
+      description: 'Only run semantic-release and related tooling',
+      default: !!process.env.TURBO_HASH,
+      defaultDescription: '"true" if run using Turbo, "false" otherwise'
+    },
+    'synchronize-interdependencies': {
+      boolean: true,
+      description:
+        'Pull latest package.json dependency versions from other packages in this project',
+      default: true
+    }
   });
 
   return {
@@ -40,11 +69,43 @@ export default function command({
     description: 'Pack and release existing production-ready distributables',
     usage: withGlobalUsage(
       `
-$1 according to the release procedure described in this project's MAINTAINING.md file.
+$1 according to the release procedure described in the MAINTAINING.md file and at length in the xscripts wiki: https://github.com/Xunnamius/xscripts/wiki. Said release procedure is essentially composed of ten tasks:
 
-The only available scope is "${ReleaseScope.ThisPackage}"; hence, when invoking this command, only the package at the current working directory will be eligible for release. Use Npm's workspace features, or Turbo's, if your goal is to potentially release multiple packages.`.trim()
+1. Validate environment variables
+2. [prerelease task] Run npm ci
+3. [prerelease task] xscripts lint --scope=this-package-source
+4. [prerelease task] xscripts build distributables
+5. [prerelease task] xscripts format
+6. [prerelease task] xscripts build documentation
+7. [prerelease task] xscripts project lint
+8. [prerelease task] xscripts test --coverage
+9. xscripts project renovate --synchronize-interdependencies (this package only)
+10. Run semantic-release and upload coverage results to Codecov
+
+Provide --ci (--continuous-integration) to enable useful functionality for CI execution environments. Specifically: run task #2, run semantic-release in CI mode, and facilitate package provenance (if using GitHub Actions). If running the release procedure by hand instead of via CI/CD, use --no-ci to disable CI-specific functionality. --no-ci (\`--ci=false\`) is the default when the NODE_ENV environment variable is undefined or "development," otherwise --ci (\`--ci=true\`) is the default.
+
+Provide --rebuild-changelog to set the XSCRIPTS_RELEASE_UPDATE_CHANGELOG environment variable in the current execution environment. This will be picked up by semantic-release, causing it to rebuild the changelog using \`xscripts build changelog\`. Provide --no-rebuild-changelog to prevent this behavior.
+
+Running \`xscripts release\` will usually execute the tasks listed above. Provide --skip-prerelease-tasks to skip running tasks #2-8, also known as the "prerelease tasks". This is useful for task scheduling tools like Turbo that decide out-of-band if/when/how to run specific prerelease tasks; such a tool only calls \`xscripts release\` when it's ready to trigger semantic-release. Therefore, \`--skip-prerelease-tasks=true\` becomes the default when Turbo is detected in the runtime environment (by checking for the existence of \`process.env.TURBO_HASH\`).
+
+The only available scope is "${ReleaseScope.ThisPackage}"; hence, when invoking this command, only the package at the current working directory will be eligible for release. Use Npm's workspace features, or Turbo's, if your goal is to potentially release multiple packages.
+
+Provide --synchronize-interdependencies to run a version of \`xscripts project renovate --synchronize-interdependencies\`, except the renovation is limited to the current package's package.json rather than every package's package.json across the entire project.
+
+This command was designed to handle concurrent execution in a safe manner. A simple file-based locking mechanism is used to ensure only one instance of this command is performing Git operations and/or running semantic-release at any given moment. If a new instance of this command begins executing before another instance has finished, the new instance will backoff and try again later.
+
+Uploading test coverage data to Codecov is only performed if any coverage data exists. A warning will be issued if no coverage data exists. Coverage data can be generated using \`xscripts test --coverage\` (which is prerelease task #8). When uploading coverage data, the package's name is used to derive a flag (https://docs.codecov.com/docs/flags). Codecov uses flags to map reports to specific packages in its UI and coverage badges.
+
+Note: this command will refuse to release a package version below 1.0.0. This is because semantic-release does not officially support packages with versions below semver 1.0.0.`.trim()
     ),
-    handler: withGlobalHandler(async function ({ $0: scriptFullName, scope }) {
+    handler: withGlobalHandler(async function ({
+      $0: scriptFullName,
+      scope,
+      ci,
+      rebuildChangelog,
+      skipPrereleaseTasks,
+      synchronizeInterdependencies
+    }) {
       const genericLogger = log.extend(scriptBasename(scriptFullName));
       const debug = debug_.extend('handler');
 
@@ -57,24 +118,40 @@ The only available scope is "${ReleaseScope.ThisPackage}"; hence, when invoking 
       genericLogger([LogTag.IF_NOT_QUIETED], 'Releasing project...');
 
       debug('scope (unused): %O', scope);
+      debug('ci: %O', ci);
+      debug('rebuildChangelog: %O', rebuildChangelog);
+      debug('skipPrereleaseTasks: %O', skipPrereleaseTasks);
+      debug('synchronizeInterdependencies: %O', synchronizeInterdependencies);
 
-      // TODO (leave turbo's tasks to turbo, only focus on releasing!)
-      // TODO (skip reinstalling node_modules if dir exists unless --force-reinstall)
-      // TODO (run "early lint" first, then "late lint" (that checks all prod deps resolvable, no extraneous deps or dev deps, no missing deps) after build completes)
-      // TODO (need flag for updating changelog or not updating changelog that also is compat with XSCRIPTS_RELEASE_UPDATE_CHANGELOG)
-      // TODO (use changelog.patch.?(cm)js if available)
-      // TODO (do codecov upload last)
-      // TODO (tests run WITHOUT --no-warnings !!!)
-      // TODO (releases use npm provenance attestation features if --with-provenance flag is given; see https://docs.npmjs.com/generating-provenance-statements)
-      // TODO (create as an issue to note for later: simultaneously publish to GitHub Packages and NPM)
-      // TODO (check early that all required environment variables are defined and valid)
-      // TODO (ensure simultaneous releases are supported)
-      // TODO (--codecov-flags to determine which flags to send to codecov when uploading test results)
-      // TODO (configure this command by configuring the appropriate npm scripts at the expected "well-known" names (same with all commands honestly))
-      // TODO (do not allow packages below version 1.0.0 to be published (semantic-release incompatibility))
-      // TODO (GPG keys generated during each major must be set to never expire... however previously active keys must be revoked (only if they maintain their "verified" status on GitHub though))
-      // TODO (need to do codecov stuff in here: CODECOV_TOKEN=$(npx --yes dotenv-cli -p CODECOV_TOKEN) npx codecov)
-      // TODO (sanity check built files to ensure all require() paths are valid)
+      // TODO: becomes part of xscripts build distributables (and project lint)
+      // TODO: sanity check built files types are correct using attw
+
+      // TODO: becomes part of xscripts build distributables (and project lint)
+      // TODO: sanity check built files to ensure all require() paths are valid
+
+      // TODO: becomes part of xscripts build distributables (and project lint)
+      // TODO: sanity check exports to ensure all entry points are valid
+
+      // TODO: becomes part of xscripts build distributables (and project lint)
+      // TODO: sanity check all node_modules imports are included among package.json dependencies and no extraneous dependencies are included (only issue warning if this last check fails)
+
+      // TODO: ensure simultaneous releases are supported (backoff if locked)
+
+      // TODO: check that all required environment variables are defined and valid
+
+      // TODO: execute tasks #1-8 wrt --ci, --rebuild-changelog, and --skip-prerelease-tasks
+
+      void process.env.XSCRIPTS_RELEASE_REBUILD_CHANGELOG;
+
+      // TODO: execute task #9 wrt --synchronize-interdependencies
+
+      // TODO: execute task #10 (semantic-release, maybe with --ci)
+      // TODO: provenance via https://docs.npmjs.com/generating-provenance-statements
+      // TODO: simultaneously publish to GitHub Packages and NPM?
+      // TODO: simultaneously publish to NPM and JSR?
+      // TODO: do not allow packages below version 1.0.0 to be published (error early in plugin)
+
+      // TODO: do codecov upload last; CODECOV_TOKEN=$(npx --yes dotenv-cli -p CODECOV_TOKEN) npx codecov; use codecov flags to determine which flags to send to codecov when uploading test results
 
       genericLogger([LogTag.IF_NOT_QUIETED], standardSuccessMessage);
     })
