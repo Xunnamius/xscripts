15bdfec2b14fe5d1ce1f71da30043407133739aa
Xunnamius (Romulus) <Xunnamius@users.noreply.github.com>
2024-10-29 02:18:36 -0700

    chore(ncurc): add commentary

--- .ncurc.js
+++ .ncurc.js
@@ -8,6 +8,7 @@ module.exports = {
   reject: [
     // ? Reject any super-pinned dependencies (e.g. find-up~5 and execa~7)
     '*~*',
+    // TODO: delete this once @-xun/debug migrates out of packages/
     // ? Pin the CJS version (for the debug package)
     'supports-color',
     // ? Until upstream semantic-release-atam updates, this needs to be frozen
