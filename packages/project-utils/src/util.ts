9c795fb36cc3b1c65231c569628d3959f5ff4366
Xunnamius (Romulus) <Xunnamius@users.noreply.github.com>
2024-11-01 03:24:46 -0700

    refactor(packages/project-utils): use contravariance to our advantage

--- packages/project-utils/src/util.ts
+++ packages/project-utils/src/util.ts
@@ -5,15 +5,13 @@ import type { SetReturnType } from 'type-fest';
  *
  * @internal
  */
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export type ParametersNoFirst<T extends (...args: any) => any> =
+export type ParametersNoFirst<T extends (...args: never) => unknown> =
   Parameters<T> extends [infer _X, ...infer Parameters_] ? Parameters_ : [];
 
 /**
  * Return the synchronous version of a function's call signature.
  */
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export type SyncVersionOf<T extends (...args: any) => any> = SetReturnType<
+export type SyncVersionOf<T extends (...args: never) => unknown> = SetReturnType<
   T,
   Awaited<ReturnType<T>>
 >;
