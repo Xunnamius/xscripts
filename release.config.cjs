62743bdeb7c9f7effc328e233666c56dfb001833
Xunnamius (Romulus) <Xunnamius@users.noreply.github.com>
2024-11-09 19:02:45 -0800

    refactor(release): migrate os random back into libraried release.config.cjs

--- release.config.cjs
+++ release.config.cjs
@@ -1,10 +1,6 @@
 // @ts-check
 'use strict';
 
-const crypto = require('node:crypto');
-const os = require('node:os');
-const path = require('node:path');
-
 const debug = require('debug')('xscripts:semantic-release-config');
 
 const { deepMergeConfig } = require('@-xun/scripts/assets');
@@ -12,18 +8,8 @@ const { moduleExport } = require('@-xun/scripts/assets/config/release.config.cjs
 
 const { parserOpts, writerOpts } = require('./conventional.config.cjs');
 
-const releaseSectionPath = path.join(
-  os.tmpdir(),
-  `xscripts-release-changelog-${crypto.randomBytes(4).readUInt32LE(0).toString(16)}.md`
-);
-
-debug(`releaseSectionPath: ${releaseSectionPath}`);
-
-module.exports = deepMergeConfig(
-  moduleExport({ releaseSectionPath, parserOpts, writerOpts }),
-  {
-    // Any custom configs here will be deep merged with moduleExport's result
-  }
-);
+module.exports = deepMergeConfig(moduleExport({ parserOpts, writerOpts }), {
+  // Any custom configs here will be deep merged with moduleExport's result
+});
 
 debug('exports: %O', module.exports);
