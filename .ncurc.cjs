138912e73356f6a10c54af15219df014ab5f0735
Xunnamius (Romulus) <Xunnamius@users.noreply.github.com>
2024-11-13 17:53:24 -0800

    chore(ncurc): update auto-update config

--- .ncurc.cjs
+++ .ncurc.cjs
@@ -3,8 +3,9 @@
 // {@xscripts/notExtraneous npm-check-updates}
 
 // * https://www.npmjs.com/package/npm-check-updates#configuration-files
-
+// @renovate
 module.exports = {
+  install: 'never',
   reject: [
     // ? Reject any super-pinned dependencies (e.g. find-up~5 and execa~7)
     '*~*',
