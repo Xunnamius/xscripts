876f175a5f74cfeb6e0819ea7f8493c26faca77a
Xunnamius (Romulus) <Xunnamius@users.noreply.github.com>
2024-11-24 09:41:46 -0800

    chore(src): stub out additional asset configs

---
+++ src/assets/config/_gac.config.mjs.ts
@@ -0,0 +1,12 @@
+import { assertIsExpectedTransformerContext, makeTransformer } from 'universe:assets.ts';
+
+import type { EmptyObject } from 'type-fest';
+
+export type Context = EmptyObject;
+
+export const { transformer } = makeTransformer<Context>({
+  transform(context) {
+    const { name } = assertIsExpectedTransformerContext(context);
+    return { [name]: '' };
+  }
+});
