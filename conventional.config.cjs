464b28d5f2e8aef8105f81d42dbb23e638c7a322
Xunnamius (Romulus) <Xunnamius@users.noreply.github.com>
2024-12-04 07:32:58 -0800

    refactor(conventional): add missing moduleExport items

--- conventional.config.cjs
+++ conventional.config.cjs
@@ -1,7 +1,18 @@
 'use strict';
 
-const { moduleExport } = require('@-xun/scripts/assets/config/conventional.config.cjs');
+const {
+  moduleExport,
+  noSpecialInitialCommitIndicator
+} = require('@-xun/scripts/assets/config/conventional.config.cjs');
+
+// TODO: delete this
+const { analyzeProjectStructure } = require('./dist/packages/project-utils/src/index.js');
 
 module.exports = moduleExport({
-  // Any custom configs here will be deep merged with moduleExport
+  configOverrides(config) {
+    // Any custom configs here will be deep merged with moduleExport
+    return config;
+  },
+  projectMetadata: analyzeProjectStructure.sync({ useCached: true }),
+  specialInitialCommit: noSpecialInitialCommitIndicator
 });
